5558798a33f3ea05f3cf2249151856680f278f0e
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2020-11-30 11:17:35 +0700

    revamped

--- Back End Notebook/Diaz CMS Project/classes/Config.php
+++ Back End Notebook/Diaz CMS Project/classes/Config.php
@@ -1,10 +1,10 @@
 <?php
 
 class Config {
-    const SMTP_HOST = "smtp.mailtrap.io";
-    const SMTP_PORT = 2525;
-    const SMTP_USER = "e842dc34c16b6b";
-    const SMTP_PASSWORD = "4e12b9abfb51e8";
+    const SMTP_HOST = null;
+    const SMTP_PORT = null;
+    const SMTP_USER = null;
+    const SMTP_PASSWORD = null;
 }
 
 ?>
